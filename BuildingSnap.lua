ffba70344cec635e167b46f6f465487a2c483aee
DumpsterDave <35818070+DumpsterDave@users.noreply.github.com>
2022-01-25 11:15:51 -0600

    debug mode

--- BuildingSnap.lua
+++ BuildingSnap.lua
@@ -23,33 +23,39 @@ BuildingSnap = {
         ["22.5"] = 45,
         ["45"] = 90,
         ["90"] = 0
-    }
+    },
+    DEBUGMODE = false
 }
 
 addModEventListener(BuildingSnap)
 
 function BuildingSnap:loadMap()
-    Logging.info(string.format("[BS] - Loading Building Snap version %s", BuildingSnap.VERSION))
+    BuildingSnap.info("Loading Building Snap version " .. BuildingSnap.VERSION)
+    if fileExists(BuildingSnap.MOD_SETTINGS .. "/buildingsnapdebugmode") then
+        BuildingSnap.info("Debug Mode Enabled = TRUE")
+        BuildingSnap.DEBUGMODE = true
+    end
     ConstructionBrushPlaceable.loadedPlaceable = Utils.appendedFunction(ConstructionBrushPlaceable.loadedPlaceable, BuildingSnap.addSnap)
     ConstructionScreen.registerBrushActionEvents = Utils.appendedFunction(ConstructionScreen.registerBrushActionEvents, BuildingSnap.registerBindings)
     ConstructionScreen.removeBrushActionEvents = Utils.appendedFunction(ConstructionScreen.removeBrushActionEvents, BuildingSnap.unregisterBindings)
+    BuildingSnap.info("Finished loading Building Snap")
 end
 
 function BuildingSnap:addSnap()
     if self.placeable.spec_placement.rotationSnapAngle ~= nil then
         if self.placeable.spec_placement.rotationSnapAngle ~= 0 then
-            Logging.info("[BS] - Building rotation snap angle already set to " .. math.deg(self.placeable.spec_placement.rotationSnapAngle))
+            BuildingSnap.debug("Building rotation snap angle already set to " .. math.deg(self.placeable.spec_placement.rotationSnapAngle))
         else
-            Logging.info("[BS] - Setting building rotation snap angle to " .. BuildingSnap.ROT_SNAP)
+            BuildingSnap.debug("Setting building rotation snap angle to " .. BuildingSnap.ROT_SNAP)
             self.placeable.spec_placement.rotationSnapAngle = math.rad(BuildingSnap.ROT_SNAP)
         end
     end
 
     if self.placeable.spec_placement.positionSnapSize ~= nil then
         if self.placeable.spec_placement.positionSnapSize ~= 0 then
-            Logging.info("[BS] - Building grid snap size already set to " .. self.placeable.spec_placement.positionSnapSize)
+            BuildingSnap.debug("Building grid snap size already set to " .. self.placeable.spec_placement.positionSnapSize)
         else
-            Logging.info("[BS] - Setting building grid snap size to " .. BuildingSnap.POS_SNAP)
+            BuildingSnap.debug("Setting building grid snap size to " .. BuildingSnap.POS_SNAP)
             self.placeable.spec_placement.positionSnapSize = BuildingSnap.POS_SNAP
         end
     end
@@ -59,7 +65,7 @@ end
 
 function BuildingSnap:registerBindings()
     local _, eventId = nil
-    Logging.info("[BS] - Registering Bindings")
+    BuildingSnap.debug("Registering Bindings")
     _, eventId = self.inputManager:registerActionEvent("BS_ADJUST_POS_SNAP", self, BuildingSnap.togglePosSnap, false, true, false, true)
     table.insert(self.brushEvents, eventId)
     self.togglePosSnapEvent = eventId
@@ -72,10 +78,11 @@ function BuildingSnap:registerBindings()
     BuildingSnap.RotSnapEvent = eventId
     self.inputManager:setActionEventTextPriority(self.toggleRotSnapEvent, GS_PRIO_HIGH)
     self.inputManager:setActionEventText(self.toggleRotSnapEvent, string.format(g_i18n:getText("BS_ADJUST_ROT_SNAP_TEXT"), tostring(BuildingSnap.ROT_SNAP)))
+    BuildingSnap.debug("Done Registering Bindings")
 end
 
 function BuildingSnap:unregisterBindings()
-    Logging.info("[BS] - De-registering Bindings")
+    BuildingSnap.debug("De-registering Bindings")
     self.togglePosSnapEvent = nil
     BuildingSnap.PosSnapEvent = nil
     self.toggleRotSnapEvent = nil
@@ -84,7 +91,7 @@ end
 
 function BuildingSnap:togglePosSnap()
     BuildingSnap.POS_SNAP = BuildingSnap.POS_SNAP_OPTIONS[tostring(BuildingSnap.POS_SNAP)]
-    Logging.info("[BS] - Position Snap Changed to " .. BuildingSnap.POS_SNAP)
+    BuildingSnap.debug("Position Snap Changed to " .. BuildingSnap.POS_SNAP)
     BuildingSnap.lastBuilding.placeable.spec_placement.positionSnapSize = BuildingSnap.POS_SNAP
     g_inputBinding:setActionEventText(BuildingSnap.PosSnapEvent, string.format(g_i18n:getText("BS_ADJUST_POS_SNAP_TEXT"), tostring(BuildingSnap.POS_SNAP)))
     g_inputBinding:setActionEventTextVisibility(BuildingSnap.PosSnapEvent, true)
@@ -92,8 +99,26 @@ end
 
 function BuildingSnap:toggleRotSnap()
     BuildingSnap.ROT_SNAP = BuildingSnap.ROT_SNAP_OPTIONS[tostring(BuildingSnap.ROT_SNAP)]
-    Logging.info("[BS] - Rotation Snap Changed to " .. BuildingSnap.ROT_SNAP)
+    BuildingSnap.debug("Rotation Snap Changed to " .. BuildingSnap.ROT_SNAP)
     BuildingSnap.lastBuilding.placeable.spec_placement.rotationSnapAngle = math.rad(BuildingSnap.ROT_SNAP)
     g_inputBinding:setActionEventText(BuildingSnap.RotSnapEvent, string.format(g_i18n:getText("BS_ADJUST_ROT_SNAP_TEXT"), tostring(BuildingSnap.ROT_SNAP)))
     g_inputBinding:setActionEventTextVisibility(BuildingSnap.RotSnapEvent, true)
+end
+
+function BuildingSnap.info(message)
+    print(string.format("[BS]::INFO    - %s", tostring(message)))
+end
+
+function BuildingSnap.warning(message)
+    print(string.format("[BS]::WARNING - %s", tostring(message)))
+end
+
+function BuildingSnap.error(message)
+    print(string.format("[BS]::ERROR  - %s", tostring(message)))
+end
+
+function BuildingSnap.debug(message)
+    if BuildingSnap.DEBUGMODE == true then
+        print(string.format("[BS]::DEBUG   - %s", tostring(message)))
+    end
 end
